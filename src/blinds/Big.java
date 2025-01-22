8df461528d73fbf384917761e97faa46809a5dc4
alex <letalexalexx@gmail.com>
2025-01-22 14:45:59 +0100

    added small and big blind

---
+++ src/blinds/Big.java
@@ -0,0 +1,9 @@
+package blinds;
+
+import java.math.BigInteger;
+
+public class Big extends Blind{
+    public Big(BigInteger chipGoal) {
+        super("Big blind", chipGoal);
+    }
+}
---
+++ src/blinds/Small.java
@@ -0,0 +1,9 @@
+package blinds;
+
+import java.math.BigInteger;
+
+public class Small extends Blind{
+    public Small(BigInteger chipGoal) {
+        super("Big blind", chipGoal);
+    }
+}
